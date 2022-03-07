fd040c8a8186af4e365279317769cf5a064aeac3
Ana Perola <anaperolinha@hotmail.com>
2022-03-07 08:40:56 -0300

    Create component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
