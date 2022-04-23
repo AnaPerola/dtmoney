20f9bf6ec322f540f55ed9a01335b44822807530
Ana Perola <anaperolinha@hotmail.com>
2022-04-22 21:55:38 -0300

    Create feature and add styles

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { TransactionTable } from "../TransactionTable";
 import { Container } from "./style";
 import { Summary } from "./Summary";
 
@@ -5,6 +6,7 @@ export function Dashboard(){
   return (
     <Container>
       <Summary />
+      <TransactionTable />
     </Container>
   )
 }
